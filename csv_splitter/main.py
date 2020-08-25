7e623e340549ee6e54e5a36bf7ee977fc90e5133
Filipe Braga <filipebragabrasil@hotmail.com>
2020-08-25 16:41:46 -0300

    Informativos no console: loading bar, tempo estimado e situação da geração de arquivos

--- csv_splitter/main.py
+++ csv_splitter/main.py
@@ -2,6 +2,7 @@ import csv
 import math
 import sys
 import pandas as pd
+from tqdm import tqdm
 
 arquivo_entrada = sys.argv[1]
 qtd_entrada = sys.argv[2]
@@ -13,9 +14,7 @@ try:
 except:
     sys.exit("Ocorreu uma falha na leitura do arquivo", file=sys.stderr)
 
-i = 0
-y = 0
-z = 0
+i = 0 ; y = 0 ; z = 0
 
 #CONFIG
 totalrows = len(open(filename).readlines()) - 1
@@ -23,6 +22,7 @@ linesPerFiles = int(qtd_entrada)
 totalFilesResult = math.ceil(totalrows / linesPerFiles)
 
 df = pd.DataFrame()
+dl = pd.DataFrame(reader)
 
 if (linesPerFiles >= totalrows) :
     print("O Número de linhas por arquivo não pode ser maior ou igual ao total de linhas do arquivo de origem.")
@@ -30,22 +30,30 @@ if (linesPerFiles >= totalrows) :
 
 onlyFilename = str(filename).split('.')[0]
 
-for row in reader :
-    if (y == linesPerFiles) :
-        y = 0
-        df.to_csv(onlyFilename + " part" + str(z) + ".csv", sep=";", index=False)
-        z = z + 1
-        df = pd.DataFrame()
+print("Gerando " + str(totalFilesResult) + " Arquivos com " + str(qtd_entrada) + " linhas")
 
-    i = i + 1
-    y = y + 1
+def separar(row) :
+        global i; global z; global y; global df
 
-    if (y <= linesPerFiles) :
-        df = df.append(row, ignore_index=True)
-
-        if (i == totalrows) :
-            z = z + 1
+        if (y == linesPerFiles) :
+            print("\n\n [ ✔ ] Arquivo '" + onlyFilename + " part" + str(z) + ".csv' gerado com " + str(y) + " linhas\n\n")
+            y = 0
             df.to_csv(onlyFilename + " part" + str(z) + ".csv", sep=";", index=False)
-   
-print("Finalizado")
+            z = z + 1
+            df = pd.DataFrame()
+
+        i = i + 1
+        y = y + 1
+
+        if (y <= linesPerFiles) :
+            df = df.append(row, ignore_index=True)
+
+            if (i == totalrows) :
+                df.to_csv(onlyFilename + " part" + str(z) + ".csv", sep=";", index=False)
+
+for index, row in tqdm(dl.iterrows(), total=dl.shape[0]):
+    separar(row)
+
+print("\n\n [ ✔ ] Arquivo '" + onlyFilename + " part" + str(z) + ".csv' gerado com " + str(y) + " linhas\n\n")
+print("Finalizado\n\n")
 sys.exit()
